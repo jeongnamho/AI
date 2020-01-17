00b4185c2ad3f3fd36576f535d067b527a63fcde
jeongnamho <jnh7807@gmail.com>
2020-01-17 17:58:34 +0900

    2020.01.17

--- OpenCV/2020.01.15/main.py
+++ OpenCV/2020.01.15/main.py
@@ -1,18 +1,35 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, send_file
 import pprint
 import urllib, cv2
 import pytesseract
 import numpy as np 
+import matplotlib.pyplot as plt
+
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
-
+def imshow(tit, image):
+    plt.title(tit)
+    if len(image.shape) == 3:
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    else :
+        plt.imshow(image, cmap='gray')
+    plt.show()
 
 
 app = Flask(__name__)
 
 # set FLASK_APP=<파일명>.py
 # flask run
+
+@app.route('/tmp/<result>.png')
+def img_file_download(result):
+    file_name = f"tmp/"+result+".png"
+    return send_file(file_name,
+                    mimetype='image/png',
+                    attachment_filename=result+'.png', # 다운받아지는 파일 이름.
+                    as_attachment=True)
+
 @app.route('/')
 def hello():
     name = request.args.get("name", "World")
@@ -47,13 +64,18 @@ def namecard():
         with open('./namecard.jpg', 'wb') as output:
             output.write(input.read())
 
+    
+
+
+
     img = cv2.imread('namecard.jpg')
     img_copy = img.copy()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img_blur = cv2.GaussianBlur(gray, (9,9), 0)
-    _, binary = cv2.threshold(img_blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    img_blur = cv2.medianBlur(gray, 21)
+    _, binary = cv2.threshold(img_blur, 80, 255, cv2.THRESH_BINARY)
     kernel = np.ones((3,3), np.uint8)
  
+
     opening = cv2.morphologyEx(binary, cv2.MORPH_OPEN, kernel, iterations=7)
  
     contours, _ = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #RETR_EXTERNAL:외곽만
@@ -71,19 +93,22 @@ def namecard():
  
     height, width = img.shape[:2]
     point_list = approx
-    pts1 = np.float32([list(point_list[1]),
-                    list(point_list[0]),
+    pts1 = np.float32([list(point_list[0]),
+                    list(point_list[1]),
                     list(point_list[2]),
                     list(point_list[3])])
  
     # print(pts1)
-    pts2 = np.float32([[0,0], [width,0], [0,height], [width,height]])
+    pts2 = np.float32([[0,0], [0,height], [width,height], [width,0]])
     # print(pts2)
     M = cv2.getPerspectiveTransform(pts1, pts2)
     # print(M)
     img_result = cv2.warpPerspective(img, M, (width, height))
+    # _, binary = cv2.threshold(img_result, 230, 255, cv2.THRESH_BINARY)
+    cv2.imwrite("namecard2.jpg", img_result)
+    
     # imshow('', img_result)
-    str = pytesseract.image_to_string(img_result)
+    str = pytesseract.image_to_string(img_result, lang="eng+kor")
     print(str)
     return {
     "version": "2.0",
@@ -97,6 +122,70 @@ def namecard():
         ]
     }
 }
+
+@app.route('/findbooks', methods=['POST'])
+def findbooks():
+    body = request.json
+    pprint.pprint(body)
+    image_url = body['action']['detailParams']['findbooks']['origin']
+
+    urllib.request.urlretrieve(image_url, "./findbooks.jpg")
+
+    print(image_url)
+    with urllib.request.urlopen(image_url) as input:
+        with open('./findbooks.jpg', 'wb') as output:
+            output.write(input.read())
+
+    image = cv2.imread("findbooks.jpg")
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gassian_blur = cv2.GaussianBlur(gray, (3, 3), 0)
+    # cv2.Canny(***, minVal, maxVal)
+    # minVal보다 작으면 엣지 아니라고 판단
+    # maxVal보다 크면 확실한 엣지
+    # 그 사이는 판단함
+    edged = cv2.Canny(gassian_blur, 10, 250) # Canny : 외곽선 정보를 알려주는 함수(edge 추출 방법)
+
+    # imshow('gaussian_blur', edged)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
+    closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
+
+    # imshow("", closed)
+    # 여기서는 closed2으로 해봄
+    cnts, _ = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    total = 0
+
+    # cv2.drawContours(image, cnts, -1, (0,0,255), 1)
+    # cv2.imwrite("output3.png")
+
+    for c in cnts:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+        if len(approx) == 4:
+            cv2.drawContours(image, [approx], -1, (0, 255, 0), 4)
+            total += 1
+    str = "I found {0} books in that image".format(total)
+    # imshow("Output", image)
+    print(str)
+    return {
+    "version": "2.0",
+    "template": {
+        "outputs": [
+            {
+                "simpleText": {
+                    "text": str
+                }
+            # {
+            #     "simpleImage": {
+            #         "imageUrl" : "http://b626c5f5.ngrok.io/tmp."+findbooks+".jpg",
+            #         "altText" : 책
+            #     }
+            }
+        ]
+    }
+}
+
+
 
 
 # def ocr(file):
