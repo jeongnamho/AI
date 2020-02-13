d7f07bc38f70a124ece1c64aa669d2f954a62f88
jeongnamho <jnh7807@gmail.com>
2020-02-13 18:19:51 +0900

    2020.02.13 (Face analysis, making continuous web page using django)

---
+++ Web/mysite/myapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('test', views.test),
+    path('login', views.login),
+    path('service', views.service),
+    path('logout', views.logout),
+    path('uploadimage', views.uploadimage),
+]
