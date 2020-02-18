58713795ca098affbd88821075c57497c499ff63
jeongnamho <jnh7807@gmail.com>
2020-02-18 18:46:54 +0900

    2020.02.18(Realizing a webpage with Django, sqlite3 that has some function such as Login, editing pages)

--- Web/mysite/mysite/settings.py
+++ Web/mysite/mysite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'myapp'
+    'myapp',
+    'blog',
 ]
 
 MIDDLEWARE = [
--- Web/mysite/mysite/urls.py
+++ Web/mysite/mysite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("blog/", include("blog.urls")),
     path("ajax/", include("ajax.urls")),
     path("", include('myapp.urls')),
     path('admin/', admin.site.urls),
